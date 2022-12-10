0bee5144ea9ab85200d4783147ec82b0ae116a5d
Mitu Cristian <104227938+mitu-cristian@users.noreply.github.com>
2022-12-10 16:34:36 +0200

    Delete JavaProjects directory

--- JavaProjects/src/emailapp/EmailApp.java
+++
@@ -1,9 +0,0 @@
-package emailapp;
-
-public class EmailApp {
-
-    public static void main (String[] args) {
-        Email eml = new Email("John", "Smith");
-        System.out.println(eml.showInfo());
-    }
-}
